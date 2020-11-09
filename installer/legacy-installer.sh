8619494c77811c75a33558c2bb1a5e36ef0de608
L4xus⚡️ <cool5tarXV@gmail.com>
2020-11-09 18:30:40 +0800

    Create legacy-installer.sh

---
+++ installer/legacy-installer.sh
@@ -0,0 +1,13 @@
+# for older versions of rootstrap migration...
+
+mkdir rootstrap
+
+mkdir rootstrap/upd8/
+
+cd rootstrap/upd8/
+
+wget https://raw.githubusercontent.com/L4xus/rootstrap/main/installer/rootstrapper.sh
+
+bash rootstrapper.sh
+
+echo done...
